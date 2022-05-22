f0b63a41a4d34d7b116bd181dc81bac23737285c
sumanmanna134 <manna.suman134@gmail.com>
2022-05-23 00:03:02 +0530

    Post comment logic, append to dom, install socket.io emit event from client,listen and broadcast event from server, listen for incoming events on client

---
+++ public/js/app.js
@@ -0,0 +1,61 @@
+let username;
+let socket = io();
+do {
+  username = prompt('Enter Username: ');
+} while (!username);
+
+const textarea = document.querySelector('#textarea');
+const submitBtn = document.querySelector('#submitBtn');
+
+const commentBox = document.querySelector('.comment__box');
+submitBtn.addEventListener('click', (currentEvent) => {
+  currentEvent.preventDefault();
+  let comment = textarea.value;
+  if (!comment) {
+    return;
+  }
+
+  postComment(comment);
+});
+
+function postComment(comment) {
+  //append to dom
+  let data = {
+    username: username,
+    comment: comment,
+  };
+  appendToDom(data);
+  //BroadCast
+  broadcastComment(data);
+  textarea.value = '';
+
+  //sync with database
+}
+function appendToDom(data) {
+  let lTag = document.createElement('li');
+  lTag.classList.add('comment', 'mb-3');
+  let markup = `
+     <div class="card border-light mb-3">
+    <div class="card-body">
+      <h6>${data.username}</h6>
+      <p>${data.comment}</p>
+      <div>
+        <img src="./img/clock.png" alt="clock" />
+        <small>${moment(data.time).format('LT')}</small>
+      </div>
+    </div>
+  </div>
+    `;
+
+  lTag.innerHTML = markup;
+  commentBox.prepend(lTag);
+}
+
+function broadcastComment(data) {
+  //Socket
+  socket.emit('comment', data);
+}
+
+socket.on('comment', (data) => {
+  appendToDom(data);
+});
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 3000;
 app.use(express.static('public'));
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`listening on port ${port}`);
+});
+let io = require('socket.io')(server);
+io.on('connection', (socket) => {
+  console.log(`new connection ${socket.id}`);
+
+  //receive event
+  socket.on('comment', (data) => {
+    data.time = Date();
+    socket.broadcast.emit('comment', data);
+  });
 });
